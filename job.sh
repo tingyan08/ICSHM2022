632ac11542a2cf01653f698ad972c03f6c9a9340
vinkc <tingyan@caece.net>
2023-04-22 00:54:57 +0800

    finish feature extraction for both sources

--- job.sh
+++ job.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#PBS -l select=1:ncpus=4:gpu_id=2
+#PBS -l select=1:ncpus=4:gpu_id=3
 ###PBS -l place=excl
 #PBS -o out.txt				
 #PBS -e err.txt				
-#PBS -N identification
-cd ~/ICSHM/Damage_identification				
+#PBS -N Extraction-accel
+cd ~/ICSHM2022			
 
 source ~/.bashrc			
 conda activate icshm	
@@ -12,11 +12,14 @@ conda activate icshm
 module load cuda-11.7			
 
 
-# python3 train_generation.py --arch cvae_paper --trainer CVAE --data_type 1D --max_epoch 1000 --description add_classfier
-# python3 train_reconstruction.py --arch unet --trainer UNet --task All --max_epoch 1000 
-# python3 train_extraction.py --arch triplet --trainer TripletDamageAE --max_epoch 500 --description Final
-python3 train_identification.py --arch classification --trainer CNN --load_model None --transfer False --max_epoch 500 --description From_scratch
-python3 train_identification.py --arch classification --trainer CNN --load_model DamageAE --transfer False --max_epoch 500 --description Pretrain_DamageAE
-python3 train_identification.py --arch classification --trainer CNN --load_model DamageAE --transfer True --max_epoch 500 --description Transfer_DamageAE
-python3 train_identification.py --arch classification --trainer CNN --load_model TripletAE --transfer False --max_epoch 500 --description Pretrain_TripletAE
-python3 train_identification.py --arch classification --trainer CNN --load_model TripletAE --transfer True --max_epoch 500 --description Transfer_TripletAE
+# python3 train_displacement_extraction.py --arch autoencoder --trainer AE --max_epoch 500 --description Final
+# python3 train_displacement_extraction.py --arch autoencoder --trainer DamageAE --max_epoch 500 --description Final
+# python3 train_displacement_extraction.py --arch autoencoder --trainer TripletAE --max_epoch 500 --description Final
+
+# python3 train_acceleration_extraction.py --arch autoencoder --trainer AE --max_epoch 500 --description Final
+
+python3 train_displacement_identification.py --arch classification --trainer CNN --load_model None --transfer False --max_epoch 500 --description From_scratch
+python3 train_displacement_identification.py --arch classification --trainer CNN --load_model DamageAE --transfer False --max_epoch 500 --description Pretrain_DamageAE
+python3 train_displacement_identification.py --arch classification --trainer CNN --load_model DamageAE --transfer True --max_epoch 500 --description Transfer_DamageAE
+python3 train_displacement_identification.py --arch classification --trainer CNN --load_model TripletAE --transfer False --max_epoch 500 --description Pretrain_TripletAE
+python3 train_displacement_identification.py --arch classification --trainer CNN --load_model TripletAE --transfer True --max_epoch 500 --description Transfer_TripletAE
