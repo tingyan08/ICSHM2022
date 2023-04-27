772a0d0e3c2416e5c04affc34b91e6719177db66
vinkc <tingyan@caece.net>
2023-04-27 22:51:11 +0800

    Update data

--- job/identification-finetune-disp.sh
+++ job/identification-finetune-disp.sh
@@ -18,4 +18,4 @@ module load cuda-11.7
 # python3 train_displacement_identification.py --arch regression --trainer CNN --load_model DamageAE --transfer False --max_epoch 500 --description Pretrain_DamageAE
 # python3 train_displacement_identification.py --arch regression --trainer CNN --load_model DamageAE --transfer True --max_epoch 500 --description Transfer_DamageAE
 # python3 train_displacement_identification.py --arch regression --trainer CNN --load_model TripletAE --transfer False --max_epoch 500 --description Pretrain_TripletAE
-python3 train_displacement_identification_finetune.py --arch regression --trainer CNN_FineTune --max_epoch 500 --description Involve_real_data
+python3 train_displacement_identification_finetune.py --arch regression --trainer CNN_FineTune --max_epoch 500 --description Remove_scenario11
